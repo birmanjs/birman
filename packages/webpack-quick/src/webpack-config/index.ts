65e46026f9fdac9fa048f6f011f888665180bc2b
wangxingkang <156148958@qq.com>
2019-11-30 14:20:32 +0800

    chore: project init

---
+++ packages/webpack-quick/src/webpack-config/index.ts
@@ -0,0 +1,5 @@
+import Config from 'webpack-chain';
+import { ProgressPlugin } from 'webpack';
+import { join, resolve, relative } from 'path';
+import { EOL } from 'os';
+import { existsSync } from 'fs';
---
+++ packages/webpack-quick/src/webpack-config/resolve-define.ts
@@ -0,0 +1,13 @@
+const prefixRE = /^BIRMAN_APP_/;
+
+interface Options {
+
+}
+
+const ENV_SHOULD_PASS = ['NODE_ENV', 'HMR', 'SOCKET_SERVER', 'ERROR_OVERLAY'];
+
+const resolveDefine = (opts: Options) => {
+
+}
+
+export default resolveDefine;
