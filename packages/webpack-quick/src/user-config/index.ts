94891f8b4e5ec9e7fab81581c28453eeef5351bd
wangxingkang <156148958@qq.com>
2019-12-22 18:06:52 +0800

    chore: 添加部分依赖

--- packages/webpack-quick/src/user-config/index.ts
+++ packages/webpack-quick/src/user-config/index.ts
@@ -1,10 +1,14 @@
+import { resolve } from 'path';
+import { existsSync, readFileSync } from 'fs';
+import assert from 'assert';
+import didyoumean from 'didyoumean2';
+import stripJsonComments from 'strip-json-comments';
+import { isPlainObject, isEqual } from 'lodash';
+
 interface Options {
   cwd: string;
-
 }
 
-const getUserConfig = () => {
-
-}
+const getUserConfig = () => {};
 
 export default getUserConfig;
