b9a1c72295f74db4483b35a7073e5b2e417d0590
wangxingkang <wang_xingkang@qq.com>
2020-08-22 17:16:42 +0800

    chore: remove test package

--- packages/test/helpers/transformers/javascript.js
+++
@@ -1,7 +0,0 @@
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-  presets: [require.resolve('@birman/babel-preset-birman/node')],
-  babelrc: false,
-  configFile: false
-});
